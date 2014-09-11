0aaf4dfe08f5c9701a2a880999cc432a1e22427c
up4479 <up4479@126.com>
2014-09-11 18:00:16 +0800

    Revert "s"
    This reverts commit 9ed917c65f4a71c9fa4efed589f9ed0860628f15.

--- M3.Website/Global.asax.cs
+++ M3.Website/Global.asax.cs
@@ -15,7 +15,7 @@ namespace M3
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles); 
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
