8b9f0d6b10e60574d101538159a1c99499b01589
Bean <Bean.B.Deng@newegg.com>
2014-09-11 15:07:56 +0800

    MVC5框架

---
+++ M3.Website/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace M3
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
