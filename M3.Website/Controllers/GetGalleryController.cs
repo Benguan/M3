94ff634bbf8563dd7a7662fac071695115abc589
Bean <Bean.B.Deng@newegg.com>
2014-09-12 10:58:56 +0800

    完成Detail API

--- M3.Website/Controllers/GetGalleryController.cs
+++ M3.Website/Controllers/GetGalleryController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using M3.Helpers;
+using M3.Models;
 
 namespace M3.Website.Controllers
 {
@@ -6,9 +8,11 @@ namespace M3.Website.Controllers
     {
         // GET: api/GetGallery/Detail/5
         [HttpGet]
-        public string Detail(int id)
+        public Category Detail(int id)
         {
-            return "value";
+            var gallery = StorageHelper.GetGallery();
+            var category = gallery.Categories.Find(c => c.Id == id);
+            return category;
         }
 
         // GET: api/GetGallery/Preview/5
