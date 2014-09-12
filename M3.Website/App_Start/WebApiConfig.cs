37c7179b08449c43dabbb2b7343ffd29cb53d923
Bean <Bean.B.Deng@newegg.com>
2014-09-12 09:54:04 +0800

    bugfix

--- M3.Website/App_Start/WebApiConfig.cs
+++ M3.Website/App_Start/WebApiConfig.cs
@@ -11,19 +11,13 @@ namespace M3.Website
             config.Routes.MapHttpRoute(
                name: "GetGalleryDetailApi",
                routeTemplate: "api/{controller}/Detail/{id}",
-               defaults: new { id = RouteParameter.Optional }
+               defaults: new { action = "Detail", id = RouteParameter.Optional }
             );
 
             config.Routes.MapHttpRoute(
                name: "GetGalleryPreviewApi",
                routeTemplate: "api/{controller}/Preview/{id}",
-               defaults: new { id = RouteParameter.Optional }
-            );
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+               defaults: new { action = "Preview", id = RouteParameter.Optional }
             );
         }
     }
